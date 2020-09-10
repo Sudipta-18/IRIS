a7f5d978ccb93497f7d13ceed90bba331108045e
ayushk780 <40500298+ayushk780@users.noreply.github.com>
2020-09-10 09:49:42 +0530

    Add files via upload

---
+++ IRIS-ringtone/android/app/src/main/kotlin/com/example/IRIS/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.IRIS
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ IRIS-ringtone/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,19 @@
+//
+//  Generated file. Do not edit.
+//
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<ringtone/RingtonePlugin.h>)
+#import <ringtone/RingtonePlugin.h>
+#else
+@import ringtone;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [RingtonePlugin registerWithRegistrar:[registry registrarForPlugin:@"RingtonePlugin"]];
+}
+
+@end
